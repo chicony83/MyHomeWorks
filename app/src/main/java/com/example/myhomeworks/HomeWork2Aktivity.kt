8d2d56b5ed8979785ba46b168ed111cc79ba49c1
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-09-18 20:49:18 +0300

    rename HomeWork2Activity.kt

--- app/src/main/java/com/example/myhomeworks/HomeWork2Aktivity.kt
+++ app/src/main/java/com/example/myhomeworks/HomeWork2Aktivity.kt
@@ -3,9 +3,9 @@ package com.example.myhomeworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class HomeWork2Aktivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_HomeWork2)
     }
 }
