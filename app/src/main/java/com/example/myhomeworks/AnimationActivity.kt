476e7b9ad1b22fffc31cbdf169bf5a54baf6e0e8
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-09-27 13:36:14 +0300

    add Animation in AnimationActivity.kt

--- app/src/main/java/com/example/myhomeworks/AnimationActivity.kt
+++ app/src/main/java/com/example/myhomeworks/AnimationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myhomeworks
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_animation.*
+
 
 class AnimationActivity : AppCompatActivity() {
+    private var mAnimationDrawable: AnimationDrawable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
+
+        var animation = imageView_catAnimation
+        animation.setBackgroundResource(R.drawable.anim)
+        mAnimationDrawable = imageView_catAnimation.background as AnimationDrawable
+        mAnimationDrawable!!.start()
     }
 }
