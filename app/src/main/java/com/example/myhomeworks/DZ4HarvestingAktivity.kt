31268fe7663d5f43ee0989e4a566adcc4b861034
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-09-29 18:46:38 +0300

    worked!!!

--- app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
+++ app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
@@ -2,10 +2,85 @@ package com.example.myhomeworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_dz_4_harvesting.*
+import kotlinx.coroutines.*
+
 
 class DZ4HarvestingAktivity : AppCompatActivity() {
+    var winner = false;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz_4_harvesting)
+
+        button_incoming_data.setOnClickListener{ run() }
+
+    }
+
+    fun run() {
+//        while (!winner) {
+
+            val data = incomingData()
+
+//        }
+    }
+
+    private fun incomingData() = runBlocking<Unit> {
+        CoroutineScope(Dispatchers.Main).launch {
+            while (!winner){
+                delay(1000)
+                val rndNum = RndNum()
+//            var c = textView_company1_corn.text
+                var num: Int = 0
+                when (rndNum) {
+                    0 -> num = textView_company1_potato.text.toString().toInt()
+                    1 -> num = textView_company1_wheat.text.toString().toInt()
+                    2 -> num = textView_company1_corn.text.toString().toInt()
+                    3 -> num = textView_company2_potato.text.toString().toInt()
+                    4 -> num = textView_company2_wheat.text.toString().toInt()
+                    5 -> num = textView_company2_corn.text.toString().toInt()
+                    6 -> num = textView_company3_potato.text.toString().toInt()
+                    7 -> num = textView_company3_wheat.text.toString().toInt()
+                    8 -> num = textView_company3_corn.text.toString().toInt()
+                }
+
+                num = num.plus(1)
+                Log.i("TAG", "new v = $num")
+
+                when (rndNum) {
+                    0 -> textView_company1_potato.text = num.toString()
+                    1 -> textView_company1_wheat.text = num.toString()
+                    2 -> textView_company1_corn.text = num.toString()
+                    3 -> textView_company2_potato.text = num.toString()
+                    4 -> textView_company2_wheat.text = num.toString()
+                    5 -> textView_company2_corn.text = num.toString()
+                    6 -> textView_company3_potato.text = num.toString()
+                    7 -> textView_company3_wheat.text = num.toString()
+                    8 -> textView_company3_corn.text = num.toString()
+                }
+                //v++
+                //checkToWin(v)
+
+            }
+        }
+
     }
 }
+
+
+private fun RndNum(): Int {
+    var rnd: Int = (0..8).random()
+
+
+    Log.i("TAG", " $rnd")
+    //if (rnd == 5) winner = true
+    return rnd
+
+}
+
+
+
+
+
