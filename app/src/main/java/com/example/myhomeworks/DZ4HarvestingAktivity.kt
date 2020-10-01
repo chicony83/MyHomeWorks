d2b5773af5b7ac70937bcd16fdf4c8e61a1fd2b2
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-01 19:11:29 +0300

    working MVVM
    in DZ4HarvestingAktivity.kt
    adding observer at all variable
    
    in IncomingData.kt
    adding all variables in methods getData and postValue

--- app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
+++ app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
@@ -15,17 +15,62 @@ open class DZ4HarvestingAktivity : AppCompatActivity() {
 
 //        val incomingData = IncomingData()
 
-        val incomingData = ViewModelProvider(this).get(IncomingData::class.java)
+        val viewModelProvider = ViewModelProvider(this).get(IncomingData::class.java)
 
-        incomingData.company1_potato.observe(this,
+        addDataInModelProviderData(viewModelProvider)
+
+        button_incoming_data.setOnClickListener {
+            viewModelProvider.incomingData()
+        }
+    }
+
+    private fun addDataInModelProviderData(viewModelProvider: IncomingData) {
+
+        viewModelProvider.company1_potato.observe(this,
             { newCounterData ->
                 textView_company1_potato.text = newCounterData.toString()
             }
         )
-
-        button_incoming_data.setOnClickListener {
-            incomingData.incomingData()
-        }
+        viewModelProvider.company1_wheat.observe(this,
+            { newCounterData ->
+                textView_company1_wheat.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company1_corn.observe(this,
+            { newCounterData ->
+                textView_company1_corn.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company2_potato.observe(this,
+            { newCounterData ->
+                textView_company2_potato.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company2_wheat.observe(this,
+            { newCounterData ->
+                textView_company2_wheat.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company2_corn.observe(this,
+            { newCounterData ->
+                textView_company2_corn.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company3_potato.observe(this,
+            { newCounterData ->
+                textView_company3_potato.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company3_wheat.observe(this,
+            { newCounterData ->
+                textView_company3_wheat.text = newCounterData.toString()
+            }
+        )
+        viewModelProvider.company3_corn.observe(this,
+            { newCounterData ->
+                textView_company3_corn.text = newCounterData.toString()
+            }
+        )
     }
 }
 
--- app/src/main/java/com/example/myhomeworks/IncomingData.kt
+++ app/src/main/java/com/example/myhomeworks/IncomingData.kt
@@ -1,12 +1,25 @@
 package com.example.myhomeworks
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
 
 class IncomingData : ViewModel() {
+
     var winner = false;
+
     var company1_potato = MutableLiveData(0)
+    var company1_wheat = MutableLiveData(0)
+    var company1_corn = MutableLiveData(0)
+
+    var company2_potato = MutableLiveData(0)
+    var company2_wheat = MutableLiveData(0)
+    var company2_corn = MutableLiveData(0)
+
+    var company3_potato = MutableLiveData(0)
+    var company3_wheat = MutableLiveData(0)
+    var company3_corn = MutableLiveData(0)
 
 
     fun incomingData() = runBlocking<Unit> {
@@ -16,68 +29,44 @@ class IncomingData : ViewModel() {
                 val company = Random().num()
                 var percentofHarvesting = getData(company)
                 percentofHarvesting = percentofHarvesting?.plus(1)
-                postValue(company, percentofHarvesting)
+                Log.i("TAG","percentofHarvesting = $percentofHarvesting")
+                Log.i("TAG","company = $company")
+                percentofHarvesting?.let { postValue(company, it) }
             }
-
-
-//            while (!winner){
-//                delay(75)
-//                val rndNum = RndNum()
-//                var num: Int = 0
-//                when (rndNum) {
-//                    0 -> num = textView_company1_potato.text.toString().toInt()
-//                    1 -> num = textView_company1_wheat.text.toString().toInt()
-//                    2 -> num = textView_company1_corn.text.toString().toInt()
-//                    3 -> num = textView_company2_potato.text.toString().toInt()
-//                    4 -> num = textView_company2_wheat.text.toString().toInt()
-//                    5 -> num = textView_company2_corn.text.toString().toInt()
-//                    6 -> num = textView_company3_potato.text.toString().toInt()
-//                    7 -> num = textView_company3_wheat.text.toString().toInt()
-//                    8 -> num = textView_company3_corn.text.toString().toInt()
-//                }
-//
-//                num = num.plus(1)
-//                Log.i("TAG", "new v = $num")
-//                if(num == 100){
-//                    winner = true
-//                    Toast.makeText(this@DZ4HarvestingAktivity, "есть победитель", Toast.LENGTH_SHORT).show()
-//                }
-//                when (rndNum) {
-//                    0 -> textView_company1_potato.text = num.toString()
-//                    1 -> textView_company1_wheat.text = num.toString()
-//                    2 -> textView_company1_corn.text = num.toString()
-//                    3 -> textView_company2_potato.text = num.toString()
-//                    4 -> textView_company2_wheat.text = num.toString()
-//                    5 -> textView_company2_corn.text = num.toString()
-//                    6 -> textView_company3_potato.text = num.toString()
-//                    7 -> textView_company3_wheat.text = num.toString()
-//                    8 -> textView_company3_corn.text = num.toString()
-//                }
-//            }
         }
     }
 
     private fun postValue(company: Int, percentofHarvesting: Int) {
         when (company) {
+
             0 -> company1_potato.postValue(percentofHarvesting)
-            else -> company1_potato.postValue(percentofHarvesting)
+            1 -> company1_wheat.postValue(percentofHarvesting)
+            2 -> company1_corn.postValue(percentofHarvesting)
+
+            3 -> company2_potato.postValue(percentofHarvesting)
+            4 -> company2_wheat.postValue(percentofHarvesting)
+            5 -> company2_corn.postValue(percentofHarvesting)
+
+            6 -> company3_potato.postValue(percentofHarvesting)
+            7 -> company3_wheat.postValue(percentofHarvesting)
+            8 -> company3_corn.postValue(percentofHarvesting)
         }
     }
 
-    private fun getData(company: Int): Int {
-        val num: Int?
-        when (company) {
-            0 -> num = company1_potato.value!!
-//            1 -> num = textView_company1_wheat.text.toString().toInt()
-//            2 -> num = textView_company1_corn.text.toString().toInt()
-//            3 -> num = textView_company2_potato.text.toString().toInt()
-//            4 -> num = textView_company2_wheat.text.toString().toInt()
-//            5 -> num = textView_company2_corn.text.toString().toInt()
-//            6 -> num = textView_company3_potato.text.toString().toInt()
-//            7 -> num = textView_company3_wheat.text.toString().toInt()
-//            8 -> num = textView_company3_corn.text.toString().toInt()
-            else -> num = company1_potato.value!!
+    private fun getData(company: Int): Int? {
+        return when (company) {
+            0 -> company1_potato.value!!
+            1 -> company1_wheat.value!!
+            2 -> company1_corn.value!!
+
+            3 -> company2_potato.value!!
+            4 -> company2_wheat.value!!
+            5 -> company2_corn.value!!
+
+            6 -> company3_potato.value!!
+            7 -> company3_wheat.value!!
+            8 -> company3_corn.value!!
+            else->Log.i("TAG","hevent value")
         }
-        return num
     }
 }
