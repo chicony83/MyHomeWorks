6f9082296f93f52ecb3f9755c28693784f19e528
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-01 18:24:30 +0300

    working with one variable MVVM
    in DZ4HarvestingAktivity.kt
    adding observer at one variable
    
    in IncomingData.kt
    adding new methods getData and postValue

--- app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
+++ app/src/main/java/com/example/myhomeworks/DZ4HarvestingAktivity.kt
@@ -9,18 +9,21 @@ import kotlinx.android.synthetic.main.activity_dz_4_harvesting.*
 open class DZ4HarvestingAktivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz_4_harvesting)
-
-
 
 //        val incomingData = IncomingData()
 
         val incomingData = ViewModelProvider(this).get(IncomingData::class.java)
 
-        button_incoming_data.setOnClickListener{
+        incomingData.company1_potato.observe(this,
+            { newCounterData ->
+                textView_company1_potato.text = newCounterData.toString()
+            }
+        )
+
+        button_incoming_data.setOnClickListener {
             incomingData.incomingData()
         }
     }
--- app/src/main/java/com/example/myhomeworks/IncomingData.kt
+++ app/src/main/java/com/example/myhomeworks/IncomingData.kt
@@ -1,16 +1,22 @@
 package com.example.myhomeworks
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
 
-class IncomingData: ViewModel() {
+class IncomingData : ViewModel() {
     var winner = false;
+    var company1_potato = MutableLiveData(0)
+
 
     fun incomingData() = runBlocking<Unit> {
         CoroutineScope(Dispatchers.Main).launch {
-            while (!winner){
+            while (!winner) {
                 delay(100)
-                val random = Random().num()
+                val company = Random().num()
+                var percentofHarvesting = getData(company)
+                percentofHarvesting = percentofHarvesting?.plus(1)
+                postValue(company, percentofHarvesting)
             }
 
 
@@ -49,5 +55,29 @@ class IncomingData: ViewModel() {
 //                }
 //            }
         }
+    }
+
+    private fun postValue(company: Int, percentofHarvesting: Int) {
+        when (company) {
+            0 -> company1_potato.postValue(percentofHarvesting)
+            else -> company1_potato.postValue(percentofHarvesting)
+        }
+    }
+
+    private fun getData(company: Int): Int {
+        val num: Int?
+        when (company) {
+            0 -> num = company1_potato.value!!
+//            1 -> num = textView_company1_wheat.text.toString().toInt()
+//            2 -> num = textView_company1_corn.text.toString().toInt()
+//            3 -> num = textView_company2_potato.text.toString().toInt()
+//            4 -> num = textView_company2_wheat.text.toString().toInt()
+//            5 -> num = textView_company2_corn.text.toString().toInt()
+//            6 -> num = textView_company3_potato.text.toString().toInt()
+//            7 -> num = textView_company3_wheat.text.toString().toInt()
+//            8 -> num = textView_company3_corn.text.toString().toInt()
+            else -> num = company1_potato.value!!
+        }
+        return num
     }
 }
