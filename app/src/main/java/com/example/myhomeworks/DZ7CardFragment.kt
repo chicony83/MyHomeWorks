6b736b177a3fc24f31f3becd1ba7c6465a20bdc7
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-16 11:24:04 +0300

    DZ7CardFragment.kt add Animation on button_bear

--- app/src/main/java/com/example/myhomeworks/DZ7CardFragment.kt
+++ app/src/main/java/com/example/myhomeworks/DZ7CardFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import kotlinx.android.synthetic.main.fragment_dz_7_card.*
 
 class DZ7CardFragment : Fragment() {
 
@@ -15,5 +18,16 @@ class DZ7CardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dz_7_card, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_bear.setOnClickListener{
+            YoYo.with(Techniques.Pulse)
+                .duration(1000)
+
+                .playOn(button_bear)
+
+        }
     }
 }
