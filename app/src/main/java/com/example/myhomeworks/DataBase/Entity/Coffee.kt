0bce449d9db364c630b6267d6d54fc2f3b1bf6a0
Roman Ulasenka <chicony83@gmail.comgit config --listgit config --global user.email chicony83@gmail.com>
2020-10-20 18:32:29 +0300

    add coffee

---
+++ app/src/main/java/com/example/myhomeworks/DataBase/Entity/Coffee.kt
@@ -0,0 +1,8 @@
+package com.example.myhomeworks.DataBase.Entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "coffee_table")
+data class Coffee()
